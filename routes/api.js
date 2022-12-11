cd4c53c42d80cc849aaa4d6343a9cf3bf6d8fe95
Jovid1242 <jovid1242@gmail.com>
2022-12-11 21:43:31 +0300

    google auth

--- routes/api.js
+++ routes/api.js
@@ -1,93 +1,133 @@
-const Router = require("express").Router;
-const router = new Router();
-const { body } = require("express-validator");
-const path = require("path");
+const Router = require('express').Router
+const router = new Router()
+const password = require('passport')
+const { body } = require('express-validator')
+const path = require('path')
 
 // controllers
-const authMiddleware = require("../middlewares/authMiddleware");
-const adminMiddleware = require("../middlewares/adminMiddleware");
-const authController = require("../controllers/authController");
-const postController = require("../controllers/postController");
-const categoryController = require("../controllers/categoryController");
-const fileController = require("../controllers/fileController");
-const userController = require("../controllers/userController");
-const bannerController = require("../controllers/bannerController");
-const likeController = require("../controllers/likeController");
-const followController = require("../controllers/followController");
+const authMiddleware = require('../middlewares/authMiddleware')
+const adminMiddleware = require('../middlewares/adminMiddleware')
+const authController = require('../controllers/authController')
+const postController = require('../controllers/postController')
+const categoryController = require('../controllers/categoryController')
+const fileController = require('../controllers/fileController')
+const userController = require('../controllers/userController')
+const bannerController = require('../controllers/bannerController')
+const likeController = require('../controllers/likeController')
+const followController = require('../controllers/followController')
 
 // auth routes
-router.post(
-  "/register",
-  body("username").isLength({ min: 3 }),
-  body("email").isLength({ min: 3 }),
-  body("password").isLength({ min: 3, max: 32 }),
-  authController.registration
-);
-router.post(
-  "/login",
-  body("email").isLength({ min: 3 }),
-  body("password").isLength({ min: 3, max: 32 }),
-  authController.login
-);
-router.get("/refresh", authController.refresh);
+// router.post(
+//   "/register",
+//   body("username").isLength({ min: 3 }),
+//   body("email").isLength({ min: 3 }),
+//   body("password").isLength({ min: 3, max: 32 }),
+//   authController.registration
+// );
+
+// router.post(
+//   "/login",
+//   body("email").isLength({ min: 3 }),
+//   body("password").isLength({ min: 3, max: 32 }),
+//   authController.login
+// );
+
+router.get('/login/succes', (req, res) => {
+    if (req.user) {
+        res.status(200).json({
+            error: false,
+            message: 'Successfuly Loged in',
+            user: req.user,
+        })
+    } else {
+        res.status(403).json({
+            error: true,
+            message: 'Not Authorized',
+        })
+    }
+})
+
+router.get('/login/failed', (req, res) => {
+    res.status(401).json({
+        error: true,
+        message: 'Log in failure',
+    })
+})
+
+router.get(
+    '/google/callback',
+    password.authenticate('google', {
+        successRedirect: process.env.CLIENT_URL,
+        failureRedirect: '/login/failed',
+    })
+)
+
+app.get('/google', passport.authenticate('google', ['profile', 'email']))
+
+router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect(process.env.CLIENT_URL)
+})
+
+router.get('/refresh', authController.refresh)
 
 // post
-router.get("/posts", postController.getAll);
-router.get("/posts/:title/:id", postController.getOne);
-router.get("/posts-page", postController.getByPage);
-router.get("/posts-popular", postController.getPopularPosts);
-router.get("/posts-resent", postController.getResentPosts);
-router.post("/post", authMiddleware, postController.create);
-router.delete("/posts/:id", postController.remove);
-router.patch("/post/:id", postController.update);
-router.post("/post-view/:id", postController.viewControll);
+router.get('/posts', postController.getAll)
+router.get('/posts/:title/:id', postController.getOne)
+router.get('/posts-page', postController.getByPage)
+router.get('/posts-popular', postController.getPopularPosts)
+router.get('/posts-resent', postController.getResentPosts)
+router.post('/post', authMiddleware, postController.create)
+router.delete('/posts/:id', postController.remove)
+router.patch('/post/:id', postController.update)
+router.post('/post-view/:id', postController.viewControll)
 
 // like post
-router.post("/post/:id/like", authMiddleware, likeController.liked);
-router.post("/post/:id/unlike", authMiddleware, likeController.unliked);
+router.post('/post/:id/like', authMiddleware, likeController.liked)
+router.post('/post/:id/unlike', authMiddleware, likeController.unliked)
 
 // follow FollowController
-router.post("/user/:id/follow", authMiddleware, followController.following);
-router.post("/user/:id/unfollow", authMiddleware, followController.unfollowing);
-router.get("/user/follow", authMiddleware, followController.getAll);
+router.post('/user/:id/follow', authMiddleware, followController.following)
+router.post('/user/:id/unfollow', authMiddleware, followController.unfollowing)
+router.get('/user/follow', authMiddleware, followController.getAll)
 
-router.get("/category/:category_id", postController.getPostsByCategory);
-router.get("/author/posts/:user_id", postController.getPostsByAuthor);
+router.get('/category/:category_id', postController.getPostsByCategory)
+router.get('/author/posts/:user_id', postController.getPostsByAuthor)
 
 // category
-router.get("/categories", categoryController.getAll);
-router.get("/categories/:id", categoryController.getOne);
-router.post("/categories", categoryController.create);
-router.delete("/categories/:id", categoryController.remove);
-router.patch("/categories/:id", categoryController.update);
+router.get('/categories', categoryController.getAll)
+router.get('/categories/:id', categoryController.getOne)
+router.post('/categories', categoryController.create)
+router.delete('/categories/:id', categoryController.remove)
+router.patch('/categories/:id', categoryController.update)
 
 // users
-router.get("/users", userController.getAll);
-router.get("/user/:id", userController.getUserById);
-router.delete("/user/:id", userController.remove);
-router.get("/get_users", userController.getByPage);
-router.get("/auth/me", authMiddleware, userController.getUser);
-router.post("/user/edit", authMiddleware, userController.updateInfo);
+router.get('/users', userController.getAll)
+router.get('/user/:id', userController.getUserById)
+router.delete('/user/:id', userController.remove)
+router.get('/get_users', userController.getByPage)
+router.get('/auth/me', authMiddleware, userController.getUser)
+router.post('/user/edit', authMiddleware, userController.updateInfo)
 
 // author
-router.get("/author/posts", authMiddleware, postController.getAuthorPosts);
-router.get("/author/:id/posts", postController.getAuthorPostsAll);
+router.get('/author/posts', authMiddleware, postController.getAuthorPosts)
+router.get('/author/:id/posts', postController.getAuthorPostsAll)
 router.delete(
-  "/author/post/:id",
-  authMiddleware,
-  postController.removeAuthorPosts
-);
+    '/author/post/:id',
+    authMiddleware,
+    postController.removeAuthorPosts
+)
 
 // banner
-router.post("/banner", bannerController.create);
-router.delete("/banner/:id", bannerController.remove);
-router.get("/banners", bannerController.getAll);
+router.post('/banner', bannerController.create)
+router.delete('/banner/:id', bannerController.remove)
+router.get('/banners', bannerController.getAll)
 
 // image file
-router.get("/image/:img", (req, res) => {
-  const fileName = req.params.img;
-  res.sendFile(path.join(__dirname + "/../uploads", "images/") + fileName);
-});
-router.post("/uploads", fileController.create);
+router.get('/image/:img', (req, res) => {
+    const fileName = req.params.img
+    res.sendFile(path.join(__dirname + '/../uploads', 'images/') + fileName)
+})
+router.post('/uploads', fileController.create)
 
-module.exports = router;
+module.exports = router
